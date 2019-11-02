6bd4d308bc5afb3b4cce3684c96f6fac6320d4f9
Horus Lugo <horusgoul@gmail.com>
2019-11-02 12:04:00 +0000

    ci(release): replace @semantic-release/git with @semantic-release/github plugin

--- release.config.js
+++ release.config.js
@@ -18,6 +18,6 @@ module.exports = {
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     '@semantic-release/npm',
-    '@semantic-release/git',
+    '@semantic-release/github',
   ],
 };
