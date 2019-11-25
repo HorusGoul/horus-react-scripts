f93cf95fc967e7cb879af55a18276695e4af9cbb
Horus Lugo <horusgoul@gmail.com>
2019-11-26 00:50:50 +0100

    Adds tests

---
+++ test/eject.test.js
@@ -0,0 +1,64 @@
+const spawn = require('cross-spawn');
+const { createSandbox } = require('./common');
+const {
+  deleteTestApp,
+  createTestApp,
+  testAppDir,
+  ejectTestApp,
+  testAppPort,
+} = createSandbox('eject');
+
+jest.setTimeout(50000);
+
+describe('Eject', () => {
+  beforeAll(done => {
+    deleteTestApp(() => {
+      createTestApp();
+      done();
+    });
+  });
+
+  afterAll(done => {
+    deleteTestApp(done);
+  });
+
+  test('it should eject an app', () => {
+    const ejectResult = ejectTestApp();
+    expect(ejectResult.status).toBe(0);
+  });
+
+  test('it should run the app', done => {
+    expect.assertions(2);
+    const start = spawn.spawn('yarn', ['start'], {
+      cwd: testAppDir,
+      env: {
+        PORT: testAppPort,
+      },
+    });
+
+    start.stdout.on('data', data => {
+      const success = data.includes('Compiled successfully.');
+      const failure = data.includes('Command failed with exit code');
+
+      if (success || failure) {
+        expect(success).toBe(true);
+        expect(failure).toBe(false);
+        start.kill();
+      }
+    });
+
+    start.on('exit', () => {
+      done();
+    });
+  });
+
+  test('it should build the app', () => {
+    const buildResult = spawn.sync('yarn', ['build'], { cwd: testAppDir });
+    expect(buildResult.status).toBe(0);
+  });
+
+  test('it should test the app', () => {
+    const testResult = spawn.sync('yarn', ['test'], { cwd: testAppDir });
+    expect(testResult.status).toBe(0);
+  });
+});
